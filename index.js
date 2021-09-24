c36d079c8587efa1feedcef9c1d74e866867f7f8
abhishek17551 <ababhishek113@gmail.com>
2021-09-24 20:10:14 +0530

    upto-second-exercise

---
+++ index.js
@@ -0,0 +1,28 @@
+const inputAngles = document.querySelectorAll(".input-angle");
+const isTriangleButton = document.querySelector("#is-triangle");
+const outputTriangle = document.querySelector("#output");
+
+
+function calculateSumOfAngles(angle1,angle2,angle3){
+    const sumOfAngles = angle1 + angle2 + angle3;
+    //console.log(sumOfAngles);
+    return sumOfAngles;
+}
+
+function isATriangle(){
+    const sumOfAngles = calculateSumOfAngles(Number(inputAngles[0].value),Number(inputAngles[1].value),Number(inputAngles[2].value));
+    // for (let i = 0; i<inputAngles.length; i++){
+    //     console.log(inputAngles[i].value);
+    // }
+    console.log(sumOfAngles);
+
+    if(sumOfAngles === 180){
+        outputTriangle.innerText = "The given angles form a triangle!";
+    }
+    else{
+        outputTriangle.innerText = "The given angles do not form a triangle";
+    }
+
+}
+
+isTriangleButton.addEventListener('click', isATriangle)
---
+++ quiz.js
@@ -0,0 +1,20 @@
+const quizForm = document.querySelector(".quiz-form");
+const submitAnswerButton = document.querySelector("#submit-answer");
+const outputResult = document.querySelector(".output-score");
+
+const correctAnswers = ["45°", "Right-angled Triangle"];
+
+function calculateScore(){
+    let score=0;
+    let index=0;
+    const formResults = new FormData(quizForm); // constructs a set of key/value pairs representing form fields and their values
+    for(let value of formResults.values()){ //returns values  contained in object
+       if (value == correctAnswers[index]){
+           score++;
+       }
+       index+=1;
+    }
+    outputResult.innerText = "Yay! Your score is " + score;
+}
+
+submitAnswerButton.addEventListener('click', calculateScore);
